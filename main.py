5488d455bfd8cb8765d9c3feb2f455fd40f0e436
shivamwondrx <shivam.kushwaha@wondrx.info>
2026-02-05 10:00:41 +0530

    My local changes

--- main.py
+++ main.py
@@ -1,3 +1,29 @@
+from mcp.server.fastmcp import FastMCP
+mcp = FastMCP("Tryingggggggg")
+
+@mcp.tool()
+def add(a, b):
+    return a+b
+
+
+@mcp.tool()
+def read_file(path: str) -> str:
+    """Read a text file"""
+    with open(path, "r") as f:
+        return f.read()
+    
+
+if __name__ == "__main__":
+    print("MCP running...")
+    mcp.run()
+
+
+
+
+print("Wohoooooo")
+
+
+
 print("Hello")
 print("Remote First Extra line - Changed 3x")
 print("Remote First Extra line - Changed 2x")
---
+++ test-mcp.py
@@ -0,0 +1,10 @@
+from mcp.server.fastmcp import FastMCP
+mcp = FastMCP("Tryingggggggg")
+
+@mcp.tool()
+def add(a, b):
+    return a+b
+
+if __
+
+print("Wohoooooo")
